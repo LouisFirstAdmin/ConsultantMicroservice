d08ace87948bdbd6afdb38aeeda8960de74ecb18
Louis Broucke <louis.broucke@first.eu>
2021-03-11 08:39:55 +0100

    Moved interface from service to data layer

--- ConsultantData/Model/IConsultantRepository.cs
+++ ConsultantData/Model/IConsultantRepository.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using ConsultantData.Model;
 
-namespace ConsultantService.Repositories
+namespace ConsultantData.Model
 {
     public interface IConsultantRepository
     {
